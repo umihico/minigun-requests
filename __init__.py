d4267858bf1ed302b53660a753083b2fd21bc812
umihico <umihico_dummy@users.noreply.github.com>
2018-11-08 15:44:57 +0900

    pypi_version18

--- __init__.py
+++ __init__.py
@@ -1,5 +1,6 @@
 from .minigun_requests import requests
 from .minigun_requests import get_output_from_url
+from .minigun_requests import get_output_from_url_iter
 from .minigun_requests import get_left_balance
 from .example import example as _example
 
